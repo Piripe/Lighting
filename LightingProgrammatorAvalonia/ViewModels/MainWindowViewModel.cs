01b7be5fd893cdc56eab7c85fc6ac0fd0310f646
Piripe <jeuxdepierre@laposte.net>
2023-06-10 19:52:37 +0200

    Update to avalonia 11.0-rc1.1

--- LightingProgrammatorAvalonia/ViewModels/MainWindowViewModel.cs
+++ LightingProgrammatorAvalonia/ViewModels/MainWindowViewModel.cs
@@ -14,12 +14,10 @@ namespace LightingProgrammatorAvalonia.ViewModels
         public string CurrentSong => "";
         public List<ColorLayerEffectItem> Effects { get; set; } = new()
         {
-            new("Static", new Bitmap(assets.Open(new Uri("avares://LightingProgrammatorAvalonia/Assets/Images/ColorEffectsToolbox/static.png"))),new StaticEffect()),
-            new("Gradient", new Bitmap(assets.Open(new Uri("avares://LightingProgrammatorAvalonia/Assets/Images/ColorEffectsToolbox/gradient.png"))),new GradientEffect()),
-            new("Blink", new Bitmap(assets.Open(new Uri("avares://LightingProgrammatorAvalonia/Assets/Images/ColorEffectsToolbox/blink.png"))),new BlinkEffect()),
-            new("Breathe", new Bitmap(assets.Open(new Uri("avares://LightingProgrammatorAvalonia/Assets/Images/ColorEffectsToolbox/breathe.png"))),new BreatheEffect()),
+            new("Static", new Bitmap(AssetLoader.Open(new Uri("avares://LightingProgrammatorAvalonia/Assets/Images/ColorEffectsToolbox/static.png"))),new StaticEffect()),
+            new("Gradient", new Bitmap(AssetLoader.Open(new Uri("avares://LightingProgrammatorAvalonia/Assets/Images/ColorEffectsToolbox/gradient.png"))),new GradientEffect()),
+            new("Blink", new Bitmap(AssetLoader.Open(new Uri("avares://LightingProgrammatorAvalonia/Assets/Images/ColorEffectsToolbox/blink.png"))),new BlinkEffect()),
+            new("Breathe", new Bitmap(AssetLoader.Open(new Uri("avares://LightingProgrammatorAvalonia/Assets/Images/ColorEffectsToolbox/breathe.png"))),new BreatheEffect()),
         };
-
-        private static IAssetLoader assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
     }
 }
